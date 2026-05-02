b7f48a357b26b9c2b51502596073a47c48c42d06
Athan <jonpuray@gmail.com>
2026-05-02 20:02:34 +0800

    removed playwright

--- install_browsers.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-# Install Playwright browsers for Streamlit Cloud
-python -m pip install --upgrade pip
-python -m playwright install chromium
-python -m playwright install-deps chromium
